f03e1b99c22324211b0a5d782e190504b40667f3
Yuta Koshizawa <koher@koherent.org>
2018-07-26 18:40:53 +0900

    Remove extension for `CGAffineTransform`
    To make SwiftyMatrix have no dependencies.

--- Sources/SwiftyMatrix/CoreGraphics.swift
+++
@@ -1,8 +0,0 @@
-#if canImport(CoreGraphics)
-import CoreGraphics
-
-public func *(lhs: CGAffineTransform, rhs: CGAffineTransform) -> CGAffineTransform {
-    return lhs.concatenating(rhs)
-}
-
-#endif
